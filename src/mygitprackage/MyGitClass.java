66bf7d6581b03a080d4daf7f2504a47e153363f0
nsolak <nurcannsolak@gmail.com>
2021-04-04 23:22:51 +0300

    4.commmit

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -3,9 +3,18 @@ package mygitprackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-	System.out.println("Versiyon 1");
-System.out.println("My second commit");
-System.out.println("My third commit");
+		System.out.println("Versiyon 1");
+		System.out.println("My second commit");
+		System.out.println("My third commit");
+		System.out.println("4. commit");	
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
