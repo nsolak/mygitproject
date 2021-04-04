1c2d4fa3293ea64fbcf6fbd6acb8791eaafffbe1
nsolak <81630697+nsolak@users.noreply.github.com>
2021-04-04 23:39:19 +0300

    Update MyGitClass.java
    duzeltildi

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -3,26 +3,10 @@ package mygitprackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Versiyon 1");
 		System.out.println("My second commit");
 		System.out.println("My third commit");
 		System.out.println("4. commit");	
 		
-		
-		
-		
-		
-		
-		
-=======
-	System.out.println("Versiyon 1");
-        System.out.println("My second commit");
-        System.out.println("My third commit");
-	System.out.println("uzak sunucudaki kodum");
-	System.out.println("yasın beyın kodu");	
->>>>>>> 662acc7ab6f49ee0d751ef3ade5745d4cae7c6ea
-		
 	}
-
 }
