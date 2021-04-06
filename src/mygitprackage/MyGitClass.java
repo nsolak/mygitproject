0b1069a6fad92f62725b19ac7e52edcd6d728d8f
nsolak <81630697+nsolak@users.noreply.github.com>
2021-04-06 13:26:38 +0300

    Update MyGitClass.java

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -6,7 +6,8 @@ public class MyGitClass {
 		System.out.println("Versiyon 1");
 		System.out.println("My second commit");
 		System.out.println("My third commit");
-		System.out.println("4. commit");	
+		System.out.println("4. commit");
+		System.out.println("yeni getirdigim dosya ");
 		
 	}
 }
